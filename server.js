51d45c213775bafaec6bd1d390188c7badc6abca
singhanushka26 <anuskasingh835@gmail.com>
2025-07-09 03:42:16 +0530

    Fixed againn

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.static(path.join(__dirname, './')));
 
 // Enhanced CORS configuration
 const corsOptions = {
-  origin: ['https://chatastro-wo.vercel.app'], 
+  origin: 'https://chatastro-wo.vercel.app', 
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
